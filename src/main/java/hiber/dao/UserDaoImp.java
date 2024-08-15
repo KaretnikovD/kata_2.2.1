318c44051b3a9511b305d69a8a2924a33a36c8da
Karetnikov Dmitrii <karetnikov64@mail.ru>
2024-08-15 14:22:48 +0300

    UserDaoImp refactored and autowiring fixed

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -5,15 +5,18 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
+    private final SessionFactory sessionFactory;
+
     @Autowired
-    private SessionFactory sessionFactory;
+    public UserDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public void add(User user) {
@@ -28,12 +31,11 @@ public class UserDaoImp implements UserDao {
     }
 
     @Override
-    public User findUserByCar(String model, int series) {
-        String hql = "from User where car.model=:model and car.series=:series";
-        Query query = sessionFactory.getCurrentSession().createQuery(hql);
+    public User getUserByCar(String model, int series) {
+        TypedQuery<User> query = sessionFactory.getCurrentSession()
+                .createQuery("from User u where u.car.model = :model and u.car.series = :series");
         query.setParameter("model", model);
         query.setParameter("series", series);
-        return (User) query.getSingleResult();
+        return query.getSingleResult();
     }
-
 }
